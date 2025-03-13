e332607a5419dbb4746695e38d040bd98628b074
MUSONDA MALANGISHA <161059335+SondaMalang@users.noreply.github.com>
2025-03-13 16:50:45 +0100

    Add files via upload

---
+++ module8/ex00/hello_all.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+class HELLO:
+ def method_hello(self):
+    print("Hello, everyone!")
+
+class_ref = HELLO()
+class_ref.method_hello() 
---
+++ module8/ex01/upcase_it.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+class UPPER:
+ def upcase_it(self):
+
+    print("Hello, everyone!").upper()
+
+class_ref = UPPER()
+class_ref.upcase_it() 
